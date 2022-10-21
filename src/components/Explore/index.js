b2efc60ee74d98c8988ccfefca9e545466556f6d
Hamza-Alhasan-22 <hamza.alhasan.22@gmail.com>
2022-10-22 01:49:03 +0300

    Edit Explore

--- src/components/Explore/index.js
+++ src/components/Explore/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css'
 import News from '../Shared/News';
 import SectionTitle from '../Shared/SectionTitle';
@@ -45,35 +45,103 @@ function Explore(props) {
             data: ''
         }
     ];
+
+    const newsComponent = news.map(item => {
+        return (
+            <News date={item.date} title={item.title}>
+                <div className='news-body'>
+                    {item.hasData ? <p>{item.data}</p> : <></>}
+                    {item.hasImage ? <img src={require('./' + item.image)} alt={'news ' + item.id} /> : <></>}
+                </div>
+            </News>
+        )
+    });
+
+    newsComponent.unshift(
+        <News date='FIELDTESTED JANUARY 19, 2017' title='Connected Clothing: Raye Padit'>
+            <div className='news-body'>
+                <p>As a voice for conscious fashion, Raye Padit is
+                    motivated by the belief that one person’s actions,
+                    no matter how small, holds great impact. This core
+                    ideal has driven him to start his three passion projects:
+                    Connected Threads Asia, PeyaR, and Swagalls. </p>
+                <img src={require('./1.png')} alt='news 1' />
+            </div>
+        </News>);
+
+    const [isShown, setIsShown] = useState([true, false, false, false, false]);
+    //newsComponent.map(i=>{return(false)})
+    const handleIsShown = (index) => {
+        setIsShown(
+            isShown.map((item, i) => {
+                return (
+                    i == index ? item = true : item = false
+                )
+            })
+        )
+    };
+
+    /////////////
+    const [windowSize, setWindowSize] = useState(getWindowSize());
+
+    useEffect(() => {
+        function handleWindowResize() {
+            setWindowSize(getWindowSize());
+        }
+
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        };
+    }, []);
+
+    function getWindowSize() {
+        const { innerWidth, innerHeight } = window;
+        return { innerWidth, innerHeight };
+    }
+    /////////////
+    const itemIndex = (i) =>{
+        let x;
+        let len = isShown.length;
+        i>=0 ? x=i%len : x=(len-(Math.abs(i)%len))%len ;
+        console.log(x);
+        return x
+    };
+    const [ind,setInd] = useState(0);
+    const handleInd = (check) => {
+        check ? setInd(ind+1) : setInd(ind-1)
+    };
+
     return (
         <div className='explore-container'>
 
             <SectionTitle title='Explore' />
 
             <div className='explore-body'>
+                <div className="LeftArrow arrowHover mobile-mode" onClick={() => {handleInd(false);handleIsShown(itemIndex(ind))}}>
+                    <div className="arrow left">
+                    </div>
+                </div>
+
+                <div className="RightArrow arrowHover mobile-mode" onClick={() => {handleInd(true);handleIsShown(itemIndex(ind))}}>
+                    <div className="arrow right">
+                    </div>
+                </div>
+
                 <div className='explore-left'>
-                    <News date='FIELDTESTED JANUARY 19, 2017' title='Connected Clothing: Raye Padit'>
-                        <div className='news-body'>
-                            <p>As a voice for conscious fashion, Raye Padit is
-                                motivated by the belief that one person’s actions,
-                                no matter how small, holds great impact. This core
-                                ideal has driven him to start his three passion projects:
-                                Connected Threads Asia, PeyaR, and Swagalls. </p>
-                            <img src={require('./1.png')} alt='news 1' />
-                        </div>
-                    </News>
+                    {
+                        windowSize.innerWidth > 1000 ? newsComponent[0] :
+                            isShown[0] ? newsComponent[0] : <></>
+                    }
                 </div>
 
                 <div className='explore-right'>
                     {
-                        news.map(item => {
+                        newsComponent.map((item, i) => {
                             return (
-                                <News date={item.date} title={item.title}>
-                                    <div className='news-body'>
-                                        {item.hasData ? <p>{item.data}</p> : <></>}
-                                        {item.hasImage ? <img src={require('./' + item.image)} alt={'news ' + item.id} /> : <></>}
-                                    </div>
-                                </News>
+                                windowSize.innerWidth > 1000 && i != 0 ? item :
+                                    i == 0 || !isShown[i] ? <></> : item
                             )
                         })
                     }
